68b551942cbee9b11d15182bc7924b2b6f2dcf10
anoushk1234 <kharangateanoushk04@gmail.com>
2023-06-10 12:33:34 +0530

    init

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,39 @@
+local options = {
+  ensure_installed = { 
+    "lua",
+    "rust",
+    "bash",
+    "html",
+    "css",
+    "c",
+    "cpp",
+    "go",
+    "zig",
+    "vim",
+    "cmake",
+    "dockerfile",
+    "toml",
+    "yaml",
+    "json",
+    "git_config",
+    "git_rebase",
+    "gitcommit",
+    "gitignore",
+    "llvm",
+    "typescript",
+    "javascript",
+    "solidity",
+    "latex"
+
+
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
